2719e44047e8c8d46e83c53209fdc3fafa0fe7d3
Aquateraneal <128927736+Aquateraneal@users.noreply.github.com>
2024-08-09 16:20:47 -0400

    Update investigator.py

--- code/exampleStrats/investigator.py
+++ code/exampleStrats/investigator.py
@@ -1,7 +1,14 @@
+import random
+import numpy as np
+
 # Investigator strategy
 # On the first twenty-four turns, goes through the eight possible trios of moves.
 # Then, for each twenty-four turns after that, plays those moves in a random order.
 # The best strat from the previous go-through gets boosted by 1, and the worst gets removed.
+#
+# By Aquateraneal
 
 def strategy(history, memory):
-  pass
+    gamelen = history.shape[1]
+    if not gamelen % 24:
+        pass
