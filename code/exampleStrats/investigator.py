11c1632b1d2a96cb4a7ebbf0180c48af3569d866
Aquateraneal <128927736+Aquateraneal@users.noreply.github.com>
2024-08-09 16:06:18 -0400

    Create investigator.py

---
+++ code/exampleStrats/investigator.py
@@ -0,0 +1,7 @@
+# Investigator strategy
+# On the first twenty-four turns, goes through the eight possible trios of moves.
+# Then, for each twenty-four turns after that, plays those moves in a random order.
+# The best strat from the previous go-through gets boosted by 1, and the worst gets removed.
+
+def strategy(history, memory):
+  pass
